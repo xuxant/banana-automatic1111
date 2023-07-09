283e0c2298ba95d0f0d8df46f2755fa26702a19d
Susanta Gautam <xuxant02@gmail.com>
2023-07-09 19:10:11 +0545

    Fix endpoint

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def init():
 
     return context
 
-@app.handler(route="/text2img")
+@app.handler(route="/sdapi/v1/txt2img")
 def handler(context: dict, request: Request) -> Response:
     body = request.json.get("body")
     # model_input = json.loads(body)
